2bb889834a5d31e0d8db354880f457f96a2102f8
Unknown <kenneth_alv@hotmail.com>
2018-03-18 11:57:20 -0600

    COntinuación de PARSER

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+
+import generated.MonkeyScanner;
+import org.antlr.v4.runtime.ANTLRInputStream;
+
+import java.io.FileReader;
+
+
 public class Main {
+    public static void main(String[] args){
+        MonkeyScanner scanner = null;
+        MiParser parser = null;
+
+        try {
+            ANTLRInputStream input = new ANTLRInputStream(new FileReader("text.txt"));
+            scanner = new MonkeyScanner(input);
+
+            parser = new MiParser(scanner);
+            parser.parse();
+        }
+        catch(Exception e){
+            System.out.println("No hay archivo");
+        }
+    }
 }
--- src/generated/MonkeyScanner.java
+++ src/generated/MonkeyScanner.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/Kenneth/IdeaProjects/MonkeyParser\MonkeyScanner.g4 by ANTLR 4.7
+// Generated from C:/Users/Kenneth/IdeaProjects/MonkeyParser/Monkey-Parser\MonkeyScanner.g4 by ANTLR 4.7
 package generated;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
